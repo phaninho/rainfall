0b4771d6dd3b4670384f8c41ade2c063f21a27af
phaninho <phaninho@live.fr>
2018-10-31 01:27:44 +0100

    add level3

---
+++ level3/pseudo_code/pseudo.c
@@ -0,0 +1,19 @@
+
+int v()
+{
+    fgets(*var);
+    printf(*var);
+    eax = *m;
+    if (eax == 0x40)
+    {
+            fwrite("Wait what?!\n", 0x1, 0xc, *stdout@@GLIBC_2.0);
+            eax = system("/bin/sh");
+    }
+    return eax;
+}
+
+void main()
+{
+    v();
+    return;
+}
