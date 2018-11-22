a5a171add4ba6e1c91a9335f4425a94a2aa64c11
phaninho <phaninho@live.fr>
2018-11-22 17:47:04 +0100

    add level9

---
+++ level9/pseudo_code/code.c
@@ -0,0 +1,40 @@
+
+N   N::operator-(void * arg0)
+{
+    edx = *(arg0 + 108);
+    eax = *(arg_4 + 108);
+    eax = edx - eax;
+    return eax;
+}
+
+N   N::operator+(void * arg0)
+{
+    edx = *(arg0 + 108);
+    eax = *(arg_4 + 108);
+    eax = edx + eax;
+    return eax;
+}
+
+int N::setAnnotationEPc(int * arg0)
+{
+    eax = memcpy(arg0 + 4, arg_4, strlen(arg_4));
+    return eax;
+}
+
+void main(int ac, char **av)
+{
+    var_8 = av;
+    if (ac <= 1)
+        _exit(1);
+    else
+    {
+        ebx = operator new(0x6c);
+        N::N(ebx);
+        var_8 = ebx;
+        ebx = operator new(0x6c);
+        N::N(ebx);
+        N::setAnnotation(var_8);
+        (**ebx)(ebx, var_8);
+    }
+    return;
+}
