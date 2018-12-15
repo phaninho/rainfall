249cf315ed389e9cd8c8aeeb707c841f4a599621
phaninho <phaninho@live.fr>
2018-12-15 16:27:13 +0100

    add bonus1

--- bonus1/pseudo_code/code.c
+++ bonus1/pseudo_code/code.c
@@ -1,15 +1,12 @@
 int main(int ac, ichar **av)
 {
-    char str;
+    char str[40];
     int i = atoi(av[1]);
 
     if (i > 9)
         return 1;
-    else
-    {
-        memcpy(str, av[2], i * 4);
-        if (i == 0x574f4c46)
-                execl("/bin/sh", 0x8048580);
-    }
+    memcpy(str, av[2], i * 4);
+    if (i == 0x574f4c46)
+            execl("/bin/sh", 0x8048580);
     return 0;
 }
